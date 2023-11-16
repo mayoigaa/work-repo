2ec62ae2e7e4a72f40082098dd1a09cbad26c2de
mayoigaa <141594861+mayoigaa@users.noreply.github.com>
2023-11-16 18:45:55 +0900

    react

---
+++ 02_React/04_react/01_react_200/001/src/components/C_Exam.js
@@ -0,0 +1,34 @@
+// C_Exam.js : 자식 컴포넌트
+import { useState } from "react";
+// rfce
+// TODO: 연습문제
+// TODO : 클릭시 아래 결과대로 코딩하세요
+// TODO : 최초화면 : "안녕하세요"
+// TODO : 결과 : "안녕"
+// TODO: 1) 바인딩 변수 정의
+
+function C_Exam() {
+    // TODO: 변수 정의 : 여기
+  // useState()
+  // 바인딩 변수 : 리액트에서는 화면에서 수정이 일어났을때 정상적으로
+  //             보이게 만들어 주는 변수
+  // 사용법 : let [변수명, 수정함수] = useState("초기값")
+  let [element, setElement] = useState("안녕하세요");
+
+  // TODO: 2) myClick() 화살표 함수 : 안녕 으로 수정
+  // nfn
+  const myClick = () => {
+    // 수정 함수 : "안녕"
+    setElement("안녕");
+  };
+  
+  return (
+    <div>
+      {element}
+      <br />
+      <button onClick={myClick}>문자열변경</button>
+    </div>
+  );
+}
+
+export default C_Exam;
